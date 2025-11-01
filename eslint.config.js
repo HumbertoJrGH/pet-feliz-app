bc2da12e57aeff3762e0055351f1d6db7a7788c8
HumbertoJrGH <dev.humbertojr@gmail.com>
2025-11-01 09:53:09 -0300

    start: preparo básico de ambiente

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,19 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+	expoConfig,
+	{
+		ignores: ["dist/*"],
+		rules: {
+			semi: ["error", "always"],
+			quotes: ["error", "double", { avoidEscape: false, allowTemplateLiterals: false }],
+			indent: ["error", "tab"],
+			"react/jsx-indent": ["error", "tab"],
+			"react/jsx-indent-props": ["error", "tab"],
+			"no-tabs": "off",
+			"no-extra-parens": ["error", "all", { "ignoreJSX": "none", "nestedBinaryExpressions": false }]
+		}
+	},
 ]);
