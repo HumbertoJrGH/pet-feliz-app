ab22357ba80d2e694add740f88f0b12c50c4234a
Daniel Lopes <danieloliv.contato@gmail.com>
2025-11-02 15:14:47 -0300

    feat: Update home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,93 @@
-import { Image } from "expo-image"
-import { Platform, StyleSheet } from "react-native"
-
-import { HelloWave } from "@/components/hello-wave"
-import ParallaxScrollView from "@/components/parallax-scroll-view"
+import { StyleSheet, View, ScrollView } from "react-native"
 import { ThemedText } from "@/components/themed-text"
 import { ThemedView } from "@/components/themed-view"
+import { TabPill } from "@/components/tab-pill"
 import { Link } from "expo-router"
+import { InputField } from "@/components/input-field"
+import { QuickAccessCard } from "@/components/quick-access-card"
+import { EmptyImage } from "@/components/ui/Card"
 
 export default function HomeScreen() {
-	return (
-		<ParallaxScrollView
-			headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-			headerImage={
-				<Image
-					source={require("@/assets/images/logo.svg")}
-					style={styles.reactLogo}
-				/>
-			}>
-			<ThemedView style={styles.titleContainer}>
-				<ThemedText type="title">Bem vindo!</ThemedText>
-				<HelloWave />
-			</ThemedView>
-			<ThemedView style={styles.stepContainer}>
-				<ThemedText type="subtitle">Step 1: Try it</ThemedText>
-				<ThemedText>
-					Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-					Press{" "}
-					<ThemedText type="defaultSemiBold">
-						{Platform.select({
-							ios: "cmd + d",
-							android: "cmd + m",
-							web: "F12",
-						})}
-					</ThemedText>{" "}
-					to open developer tools.
-				</ThemedText>
-			</ThemedView>
-			<ThemedView style={styles.stepContainer}>
-				<Link href="/modal">
-					<Link.Trigger>
-						<ThemedText type="subtitle">Step 2: Explore</ThemedText>
-					</Link.Trigger>
-					<Link.Preview />
-					<Link.Menu>
-						<Link.MenuAction title="Action" icon="cube" onPress={() => alert("Action pressed")} />
-						<Link.MenuAction
-							title="Share"
-							icon="square.and.arrow.up"
-							onPress={() => alert("Share pressed")}
-						/>
-						<Link.Menu title="More" icon="ellipsis">
-							<Link.MenuAction
-								title="Delete"
-								icon="trash"
-								destructive
-								onPress={() => alert("Delete pressed")}
-							/>
-						</Link.Menu>
-					</Link.Menu>
-				</Link>
+	const cards = [
+		{ id: 1, label: "Serviços", icon: "pets", href: "cardDetails" },
+		{
+			id: 2,
+			label: "Produtos",
+			icon: "shopping-cart",
+			href: "cardDetails",
+		},
+		{ id: 3, label: "Fidelidade", icon: "", href: "/" },
+		{ id: 4, label: "Perfil", icon: "manage-accounts", href: "profile" },
+	]
 
-				<ThemedText>
-					{"Tap the Explore tab to learn more about what's included in this starter app."}
+	return (
+		<ThemedView style={styles.mainContainer}>
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<View style={styles.componentSection}>
+					<InputField
+						placeholder="Pesquisar"
+						icon="search"
+						iconPosition="right"
+					/>
+				</View>
+
+				<View style={styles.componentSection}>
+					<EmptyImage
+						style={{ width: "100%", height: 200, borderRadius: 5 }}
+					/>
+				</View>
+
+				<View style={styles.filterOptions}>
+					<TabPill title="Tudo" isActive />
+					<TabPill title="Banho" />
+					<TabPill title="Tosa" />
+					<TabPill title="Consulta" />
+				</View>
+
+				<ThemedText type="title" style={styles.title}>
+					Acesso rápido
 				</ThemedText>
-			</ThemedView>
-			<ThemedView style={styles.stepContainer}>
-				<ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-				<ThemedText>
-					{"When you're ready, run "}
-					<ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{" "}
-					<ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{" "}
-					<ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-					<ThemedText type="defaultSemiBold">app-example</ThemedText>.
-				</ThemedText>
-			</ThemedView>
-		</ParallaxScrollView>
+
+				<View style={styles.quickAccessRow}>
+					{cards.map((card) => (
+						<Link key={card.id} href={card.href} asChild>
+							<QuickAccessCard
+								icon={card.icon}
+								label={card.label}
+								comingSoon={card.label === "Fidelidade"}
+							/>
+						</Link>
+					))}
+				</View>
+			</ScrollView>
+		</ThemedView>
 	)
 }
 
 const styles = StyleSheet.create({
-	titleContainer: {
-		flexDirection: "row",
-		alignItems: "center",
-		gap: 8,
+	componentSection: {
+		marginBottom: 16,
+		gap: 12,
 	},
-	stepContainer: {
-		gap: 8,
-		marginBottom: 8,
-	},
-	reactLogo: {
+	mainContainer: {
+		padding: 16,
+		paddingTop: 32,
 		height: "100%",
-		width: "100%",
-		top: 0,
-		left: 0,
-		position: "absolute",
+	},
+	filterOptions: {
+		flexDirection: "row",
+		justifyContent: "center",
+		gap: 2,
+		marginBottom: 20,
+	},
+	title: {
+		color: "#A1CEDC",
+		fontSize: 20,
+		marginBottom: 12,
+	},
+	quickAccessRow: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		flexWrap: "wrap",
 	},
 })
